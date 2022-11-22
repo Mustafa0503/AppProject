ec84bc30535eae0b6b9547bed6e550fe4f7e1cc9
bahar <baharoya2003>
2022-11-22 13:18:10 -0500

    login and register

--- app/src/main/java/com/example/degreeplanner/Register.java
+++ app/src/main/java/com/example/degreeplanner/Register.java
@@ -3,12 +3,45 @@ package com.example.degreeplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    EditText mfullname, mEmail, mPassword, mstudentnumber;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mfullname = findViewById(R.id.FullName);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        mstudentnumber = findViewById(R.id.StudentNumber);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.createText);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+
+            }
+        }
     }
 }
