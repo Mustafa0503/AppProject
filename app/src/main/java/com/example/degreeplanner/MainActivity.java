fe212b4f52d9804b39c6c67c2cf5ff07292d50ba
bahar <baharoya2003>
2022-11-26 14:22:13 -0500

    bahar

---
+++ app/src/main/java/com/example/degreeplanner/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.degreeplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+
+    public void test(View view) {
+        Intent intent = new Intent(this, CourseLineBuilder.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
