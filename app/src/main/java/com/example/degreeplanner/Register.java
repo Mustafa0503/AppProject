0e7ed95510870796e839f7a845301b35a3f30efc
Minjun1Kim <minjun3759@gmail.com>
2022-11-26 18:24:16 -0500

    Refactored code

--- app/src/main/java/com/example/degreeplanner/Register.java
+++ app/src/main/java/com/example/degreeplanner/Register.java
@@ -120,7 +120,7 @@ public class Register extends AppCompatActivity {
                 }
 
                 if (isStudentBox.isChecked()) {
-                    startActivity(new Intent(getApplicationContext(), CourseLineBuilder.class));
+                    startActivity(new Intent(getApplicationContext(), select.class));
                     finish();
                 }
             }).addOnFailureListener(e -> {
