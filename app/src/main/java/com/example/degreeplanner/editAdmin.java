a4b04aef13edffc3c1a56803c6b96f608faa7425
bahar <baharoya2003>
2022-12-01 13:39:53 -0500

    12/1

--- app/src/main/java/com/example/degreeplanner/editAdmin.java
+++ app/src/main/java/com/example/degreeplanner/editAdmin.java
@@ -1,43 +1,92 @@
 package com.example.degreeplanner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.MotionEffect;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class editAdmin extends AppCompatActivity {
-//    private RecyclerView admincourselist;
-//    private FirebaseFirestore mFireStore;
+
+    ArrayList<String> allCourses= new ArrayList<String>();
+    static String Itemval;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseFirestore.getInstance().collection("course").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        allCourses.add(document.getString("Course Code"));
+                    }
+
+                    dothis();
+                }
+                else {
+                    Log.d(MotionEffect.TAG, "Error getting documents: ", task.getException());
+                }
+
+            }
+        });
+    }
+
+    public static String getItemval(){
+        return Itemval;
+    }
+    public void dothis(){
         setContentView(R.layout.activity_edit_admin);
-        ListView adminlist = findViewById(R.id.adminlistview);
+        ListView listView = findViewById(R.id.adminlistview);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,allCourses);
+        listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                 Itemval = (String) listView.getItemAtPosition(i);
+//                noteRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                        if(documentSnapshot.exists()){
+//                        String CourseID = noteRef.getId();
+//                        ob.update_course(CourseID);
+//                        }
+//                    }
+//                });
+                startActivity(new Intent(editAdmin.this, editCourseAdmin.class));
+                finish();
+            }
+        });
 
-//        mFireStore = FirebaseFirestore.getInstance();
-
-//        mFireStore.collection("course").addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-//                for(DocumentChange doc: value.getDocumentChanges()){
-//                    String course_name = doc.getString("Course Code");
-//                }
-//            }
-//        });
-
+    }
+    public void gg(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+        finish();
     }
 }
