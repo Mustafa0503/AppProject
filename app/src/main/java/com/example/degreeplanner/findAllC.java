e2d83d52a539eebfa27e44db223a883cc28d70b0
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2022-12-03 23:12:29 -0500

    12/3

--- app/src/main/java/com/example/degreeplanner/findAllC.java
+++ app/src/main/java/com/example/degreeplanner/findAllC.java
@@ -9,6 +9,8 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.util.ArrayList;
 
 public class findAllC extends AppCompatActivity {
@@ -77,38 +79,83 @@ public class findAllC extends AppCompatActivity {
                         result.add(target.get(i));
                     }
                     removeDup(result);
-
-                    //remove from taken courses list as well
                     System.out.println(result);
+                    //remove from taken courses list as well
                     ArrayList<ArrayList<String>> empty = new ArrayList<ArrayList<String>>();
-                    for(int i=0;i<result.size();i++){
-                        int pos = 0;
-                        pos = find(allC, result.get(i));
-                        if(allC.get(pos).pre.isEmpty()||allC.get(pos).pre.get(0).equals("None"))
-                        {
-                            //also check if its taken already by the student
-                            ArrayList<String> sum = new ArrayList<String>();
-                            if(allC.get(pos).offerSession.contains("Summer"))
-                            {
-                                //ArrayList<String> sum = new ArrayList<String>();
-                                sum.add(allC.get(pos).courseCode);
-                                empty.add(sum);
-                            }
-                            else if(allC.get(pos).offerSession.contains("Fall"))
-                            {
-                                //ArrayList<String> fal = new ArrayList<String>();
-                                sum.add(allC.get(pos).courseCode);
-                                empty.add(sum);
-                            }
-                            else
-                            {
-                               // ArrayList<String> win = new ArrayList<String>();
-                                sum.add(allC.get(pos).courseCode);
-                                empty.add(sum);
-                            }
-                        }
+                    //System.out.println(result);
+                    ArrayList<ArrayList<info>> allCases = new ArrayList<ArrayList<info>>();
+                    ArrayList<ArrayList<info>> display = new ArrayList<ArrayList<info>>();
+                    ArrayList<info> timeLine = new ArrayList<info>();
+                    //timeLine.add(new info("F", 21, "CSCA08"));
+                    //System.out.println(timeLine.get(0).offerSession);
+                    for(int i=0;i<target.size();i++)
+                    {
+                        display = singleTimeLine(allCases, timeLine, result,target.get(i),allC);
                     }
-                    System.out.println(empty);
+                    System.out.println(display.size());
+
+                    for(int i=0;i<display.size();i++)
+                    {
+                        System.out.println("**********************");
+                        for(int j=0;j<display.get(i).size();j++) {
+                            System.out.println(display.get(i).get(j).courseCode);
+                        }
+                        System.out.println("**********************");
+                        System.out.println("\n");
+                    }
+
+
+
+
+
+//                    ArrayList<ArrayList<String>> empty = new ArrayList<ArrayList<String>>();
+//                    for(int i=0;i<result.size();i++){
+//                        int pos = 0;
+//                        pos = find(allC, result.get(i));
+//                        if(allC.get(pos).pre.isEmpty()||allC.get(pos).pre.get(0).equals("None"))
+//                        {
+//                            //also check if its taken already by the student
+//                            ArrayList<String> sum = new ArrayList<String>();
+//                            ArrayList<String> fal = new ArrayList<String>();
+//                            ArrayList<String> win = new ArrayList<String>();
+//                            if(allC.get(pos).offerSession.contains("Fall"))
+//                            {
+//
+//                                fal.add(allC.get(pos).courseCode);
+//                               // empty.add(sum);
+//                            }
+//                            else if(allC.get(pos).offerSession.contains("Winter"))
+//                            {
+//
+//                                win.add(allC.get(pos).courseCode);
+//                                //empty.add(fal);
+//                            }
+//                            else
+//                            {
+//
+//                                sum.add(allC.get(pos).courseCode);
+//                                //empty.add(win);
+//                            }
+//                            int pp=0;
+//                            if(!fal.isEmpty()) {
+//
+//                                empty.add(pp,fal);
+//                                pp++;
+//
+//                            }
+//                            if(!win.isEmpty()) {
+//                                empty.add(pp,win);
+//                                pp++;
+//                            }
+//                            if(!sum.isEmpty()){
+//                                empty.add(pp,sum);
+//                                pp++;
+//
+//                            }
+//                            pp=0;
+//                        }
+//                    }
+
 
 
 
@@ -132,6 +179,37 @@ public class findAllC extends AppCompatActivity {
             }
         });
 
+    }
+    public ArrayList<ArrayList<info>> singleTimeLine(ArrayList<ArrayList<info>> allPos,ArrayList<info> timeLine,
+                                                     ArrayList<String>allPre,
+                                                     String target,ArrayList<findAllC> fire){
+        //ArrayList<info> timeLine = new ArrayList<info>();
+        for(int i=0;i<allPre.size();i++)
+        {
+            System.out.println(allPre.get(i));
+            System.out.println(allPre);
+            int position = 0;
+            position = find(fire, target);
+            if(!allPre.get(i).equals(target)&&fire.get(position).pre.contains(allPre.get(i))) {
+                int pos = 0;
+                pos = find(fire, allPre.get(i));
+                if (fire.get(pos).pre.isEmpty() || fire.get(pos).pre.get(0).equals("None")) {
+
+                    timeLine.add(new info("undefined", -1, fire.get(pos).courseCode));
+                    timeLine.clear();
+                    continue;
+                }
+                for (int j = 0; j < fire.get(pos).pre.size(); j++) {
+                    if (allPre.contains(fire.get(pos).pre.get(j)))
+                    {
+                        timeLine.add(new info("undefined", -1, fire.get(pos).courseCode));
+                        allPos.addAll(singleTimeLine(allPos,timeLine,allPre,fire.get(pos).pre.get(j),fire));
+                    }
+                    allPos.add(timeLine);
+                }
+            }
+        }
+        return allPos;
     }
     public ArrayList<String> rec(ArrayList<String> target, ArrayList<findAllC> fire){
         ArrayList<String> result = new ArrayList<String>();
@@ -163,6 +241,7 @@ public class findAllC extends AppCompatActivity {
 
             }
         }
+
 //        for(int i=0;i<result.size();i++)
 //        {
 //            for(int j=i+1;j<result.size();j++)
@@ -187,17 +266,71 @@ public class findAllC extends AppCompatActivity {
         return 0;
     }
     public void removeDup(ArrayList<String> target){
-        for(int i=0;i<target.size();i++)
+        ArrayList<String> ref = new ArrayList<>();
+        ref = target;
+        for(int i=0;i<ref.size();i++)
         {
-            for(int j=i+1;j<target.size();j++)
+            for(int j=i+1;j<ref.size();j++)
             {
-                if(target.get(i).equals(target.get(j)))
+                if(ref.get(i).equals(ref.get(j)))
                 {
+
                     target.remove(j);
                 }
             }
         }
     }
+    public ArrayList<ArrayList<String>> getTimeLine(ArrayList<String> result){
+        ArrayList<ArrayList<String>> empty = new ArrayList<ArrayList<String>>();
+        for(int i=0;i<result.size();i++){
+            int pos = 0;
+            pos = find(allC, result.get(i));
+            if(allC.get(pos).pre.isEmpty()||allC.get(pos).pre.get(0).equals("None"))
+            {
+                //also check if its taken already by the student
+                ArrayList<String> sum = new ArrayList<String>();
+                ArrayList<String> fal = new ArrayList<String>();
+                ArrayList<String> win = new ArrayList<String>();
+                if(allC.get(pos).offerSession.contains("Fall"))
+                {
+
+                    fal.add(allC.get(pos).courseCode);
+                    // empty.add(sum);
+                }
+                else if(allC.get(pos).offerSession.contains("Winter"))
+                {
+
+                    win.add(allC.get(pos).courseCode);
+                    //empty.add(fal);
+                }
+                else
+                {
+
+                    sum.add(allC.get(pos).courseCode);
+                    //empty.add(win);
+                }
+                int pp=0;
+                if(!fal.isEmpty()) {
+
+                    empty.add(pp,fal);
+                    pp++;
+
+                }
+                if(!win.isEmpty()) {
+                    empty.add(pp,win);
+                    pp++;
+                }
+                if(!sum.isEmpty()){
+                    empty.add(pp,sum);
+                    pp++;
+
+                }
+                pp=0;
+            }
+        }
+        return empty;
+    }
+
     public String getCourseCode(){
         return this.courseCode;
     }
