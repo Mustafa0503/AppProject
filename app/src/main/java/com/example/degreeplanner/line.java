0631d2bdf84f123a918988ac612f25a45bed853b
bahar <baharoya2003>
2022-12-05 22:54:54 -0500

    12/5

--- app/src/main/java/com/example/degreeplanner/line.java
+++ app/src/main/java/com/example/degreeplanner/line.java
@@ -80,9 +80,10 @@ public class line extends AppCompatActivity {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         int month = calendar.get(Calendar.MONTH);
 
-        if (month >= 5 && (month <= 8 || (month == 9 && day <= 15))) {
+        if (month >= 5 && month < 9) {
+            //starting fall
             return 0;
-        } else if ((month >= 9 && day > 15) && (month <= 12 || month == 1)) {
+        } else if (month >= 9  && month <= 12 ) {
             return 1;
         } else {
             return 2;
@@ -266,53 +267,22 @@ public class line extends AppCompatActivity {
 
             LinkedHashMap<String ,ArrayList<String>> time = new LinkedHashMap<>();
 
-            int start = getDate();
+            //int start = getDate();
             int year = Calendar.getInstance().get(Calendar.YEAR);
             int month = Calendar.getInstance().get(Calendar.MONTH);
 
-            if(start==0){
-                year = year + 1;
-                for(int i=0; i<result.size();i++)
-                {
-                    year+=i;
-                    time.put((year) + " Winter", new ArrayList<String>());
-                    time.put((year) + " Summer", new ArrayList<String>());
-                    time.put((year) + " Fall", new ArrayList<String>());
-                }
-            } else if (start == 1){
-
-                if(month!=1){
-                    year=year+1;
-                }
-
-                for(int i=0; i<result.size();i++)
-                {
-                    time.put((year) + " Summer", new ArrayList<String>());
-                    time.put((year) + " Fall", new ArrayList<String>());
-                    year+=i;
-                    time.put((year) + " Winter", new ArrayList<String>());
-                }
-
-            } else {
-                year = year + 1;
-                for(int i=0; i<result.size();i++)
-                {
-                    //time.put((year+i) + " Winter", new ArrayList<String>());
-
-                    time.put((year) + " Fall", new ArrayList<String>());
-                    year+=i;
-                    time.put((year) + " Winter", new ArrayList<String>());
-                    time.put((year) + " Summer", new ArrayList<String>());
-                }
+            if(month!=1){
+                year=year+1;
             }
 
+            for(int i=0; i<result.size();i++)
+            {
+                time.put((year) + " Winter", new ArrayList<String>());
+                time.put((year) + " Summer", new ArrayList<String>());
+                time.put((year) + " Fall", new ArrayList<String>());
+                year++;
+            }
 
-//            for(int i=3; i<result.size()+3;i++)
-//            {
-//                time.put("202"+i + " Winter", new ArrayList<String>());
-//                time.put("202"+i + " Summer", new ArrayList<String>());
-//                time.put("202"+i + " Fall", new ArrayList<String>());
-//            }
 
             ArrayList<String> takenCourses = new ArrayList<String>();
             empty = getSameTime(emptyy,result,result.size(),takenCourses);
